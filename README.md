0eccea086d465782b7ecf837935530ce16adf502
YKL-27 <ykliang88@gmail.com>
2025-12-14 23:20:38 +0800

    Release

---
+++ README.md
@@ -0,0 +1,63 @@
+# Hotel FAQ Chatbot using Multinomial Logical Regression
+
+### Overview
+- This project implements a machine-learning-based FAQ chatbot for an imaginary hotel named Astra Imperium Hotel, located in Kuala Lumpur.
+- The chatbot operates in the hotel websites to answer users' queries
+- The chatbot classifies user queries into predefined intents using Multinomial Logistic Regression, enabling fast and accurate responses to common hotel-related questions.
+
+### Software Requirements
+- Python 3.12.8
+
+### Setup Instructions
+1. Download or clone this project
+2. Go to terminal in the project directory or virtual enviroment
+3. Install dependencies: <code>pip install -r requirements.txt</code>
+4. Run training.ipynb to train the model on the data in the "data" folder and save the trained artifacts in the "joblib" folder.
+5. Run the <a href="">streamlit app</a>, keep in mind it loads pretrained joblib file stored in this repository
+
+### Customizing Responses
+- All chatbot responses are stored in the response folder:
+  - response.json — maps intents to default chatbot replies
+  - data.json — contains the dataset used for training
+- To modify or add responses:
+  1. Open the JSON file in any text editor.
+  2. Edit existing responses or add new entries following the current structure.
+  3. Save the file and retrain the model (if training data was updated) using training.ipynb.
+- Changes in response.json will immediately reflect in the chatbot without retraining.
+
+### List of all 35 intents
+- add_night
+- book_hotel
+- book_parking_space
+- bring_pets
+- cancel_hotel_reservation
+- cancellation_fees
+- change_hotel_reservation
+- check_child_policy
+- check_functions
+- check_hotel_facilities
+- check_hotel_offers
+- check_hotel_prices
+- check_hotel_reservation
+- check_in
+- check_lost_item
+- check_menu
+- check_nearby_attractions
+- check_out
+- check_payment_methods
+- check_room_availability
+- check_room_type
+- check_smoking_policy
+- customer_service
+- file_complaint
+- get_refund
+- goodbye
+- greeting
+- host_event
+- human_agent
+- invoices
+- leave_review
+- redeem_points
+- search_hotel
+- shuttle_service
+- store_luggage
---
+++ main.py
@@ -0,0 +1,104 @@
+import json
+import joblib
+import os
+import time
+import numpy as np
+import streamlit as st
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+INTENT_MODEL_PATH = os.path.join(BASE_DIR, "joblib", "intent_model.joblib")
+INTENT_VECTORIZER_PATH = os.path.join(BASE_DIR, "joblib", "intent_vectorizer.joblib")
+RESPONSES_PATH = os.path.join(BASE_DIR, "response", "responses.json")
+HOTEL_DATA_PATH = os.path.join(BASE_DIR, "response", "hotel-data.json")
+
+PROBABILITY_THRESHOLD = 0.45
+
+#! Load model + vectorizer
+try:
+    model = joblib.load(INTENT_MODEL_PATH)
+    vectorizer = joblib.load(INTENT_VECTORIZER_PATH)
+except FileNotFoundError:
+    st.error("Model or vectorizer file not found.")
+    st.stop()
+
+#! Load json
+try:
+    with open(RESPONSES_PATH, "r") as f:
+        responses = json.load(f)
+except FileNotFoundError:
+    st.error("Responses file not found.")
+    st.stop()
+
+try:
+    with open(HOTEL_DATA_PATH, "r") as f:
+        hotel_data = json.load(f)
+except FileNotFoundError:
+    st.error("Hotel data file not found.")
+    st.stop()
+
+#! Chatbot reply function
+def predict_intent(user_input):
+    user_input = user_input.lower()
+
+    start_time = time.time()
+
+    vec = vectorizer.transform([user_input])
+    intent = model.predict(vec)[0]
+
+    # confidence score
+    proba = model.predict_proba(vec)[0]
+    confidence = float(np.max(proba))
+    max_proba = proba.max()
+    if max_proba < PROBABILITY_THRESHOLD:
+        intent = "unknown_intent"
+
+    # chatbot response
+    response = responses.get(intent, responses.get("unknown_intent"))
+    if isinstance(response, list):
+        response = response[0]
+
+    elapsed = round((time.time() - start_time) * 1000, 2)
+
+    return {
+        "intent": intent,
+        "confidence": confidence,
+        "response": response.format(**hotel_data),
+        "time_ms": elapsed
+    }
+
+#! Streamlit app
+def main():
+    st.set_page_config(page_title="Hotel FAQ Chatbot", layout="centered")
+    st.title("Astra Imperium FAQ Chatbot (Logistic Regression)")
+    st.caption("Ask me about room prices, check-in times, or facilities!")
+
+    # initialize session state
+    if "messages" not in st.session_state:
+        st.session_state.messages = [{"role": "assistant", "content": "Hello! How can I help you with your hotel booking today?"}]
+
+    # display chat history
+    for msg in st.session_state.messages:
+        with st.chat_message(msg["role"]):
+            st.markdown(msg["content"])
+
+    # handle user input
+    if prompt := st.chat_input("Type your message here..."):
+        # display user message
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        responses = predict_intent(prompt)
+
+        # display chatbot reply
+        st.session_state.messages.append({"role": "assistant", "content": responses['response']})
+        with st.chat_message("assistant"):
+            st.markdown(responses['response'])
+            st.caption(
+                f"**Predicted Intent:** `{responses['intent']}` | "
+                f"**Confidence:** {responses['confidence']*100:.2f}% | "
+                f"**Time Taken:** {responses['time_ms']} ms"
+            )
+    
+if __name__ == '__main__':
+    main()
